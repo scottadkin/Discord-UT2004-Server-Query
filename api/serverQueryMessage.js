1bd3c69934fab1f47bb66eb4ecdf856c2906c2c6
Scott Adkin <scottadkin001@gmail.com>
2022-04-03 23:23:22 +0100

    testing embed

---
+++ api/serverQueryMessage.js
@@ -0,0 +1,31 @@
+class serverQueryMessage{
+
+    constructor(MessageEmbed, channel){
+
+
+        const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Server Response from: ')
+       // .setURL('https://discord.js.org/')
+        .setAuthor({ name: 'UT2004 Server Query', iconURL: 'https://i.imgur.com/ihsPMOD.png', url: 'https://github.com/scottadkin/Discord-UT2004-Server-Query' })
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/ihsPMOD.png')
+        /*.addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: '**Join as Player:** <ut2004://www.google.com>', value: '\u200B', inline: false },
+            { name: '**Join as Spectator:** <ut2004://www.google.com>', value: '\u200B', inline: false },
+        )*/
+        .addField(`Join Server`,`**<ut2004://www.google.com>**`, false)
+        .addField(`Join Server as Spectator`,`**<ut2004://www.google.com?spectatorOnly=1>**`, false)
+       // .addField('Inline field title', 'Some value here', true)
+        //.setImage('https://i.imgur.com/ihsPMOD.png')
+        .setTimestamp()
+        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/ihsPMOD.png' });
+
+        channel.send({"embeds": [exampleEmbed]});
+    }
+}
+
+module.exports = serverQueryMessage;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 const { token } = require("./config.json");
-const UT2K4Query = require("./api/ut2k4query.js");
+const UT2K4Query = require("./api/ut2k4query");
+const serverQueryMessage = require("./api/serverQueryMessage");
 //80.4.151.145
 //74.91.115.167
 const testIp = "80.4.151.145";
@@ -16,13 +17,14 @@ const discordOptions = {
 
 
 const testServer = new UT2K4Query();
-
+/*
 testServer.fetchBasicInfo(testIp, testPort + 1);
 testServer.fetchGameInfo(testIp, testPort + 1);
 testServer.fetchPlayerInfo(testIp, testPort + 1);
+*/
 
 
-/*
+
 // Create a new client instance
 const client = new Client(discordOptions);
 
@@ -33,8 +35,34 @@ client.once("ready", () => {
 
 
 client.on("messageCreate", async message =>{
+
     console.log(message);
+
+    if(message.content === "test"){
+
+        /*const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+        .addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+        )
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/AfFp7pu.png')
+        .setTimestamp()
+        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+        message.channel.send({"embeds": [exampleEmbed]});*/
+
+        new serverQueryMessage(MessageEmbed, message.channel);
+    }
 });
 
 // Login to Discord with your client"s token
-client.login(token);*/
+client.login(token);
